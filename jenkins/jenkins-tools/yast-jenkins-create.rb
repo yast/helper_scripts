b098366b2f409dbf0a63084192280bb373167abb
Josef Reidinger <jreidinger@suse.cz>
2018-04-26 12:33:12 +0200

    update script and use crumb with link how to get it

--- jenkins/jenkins-tools/yast-jenkins-create.rb
+++ jenkins/jenkins-tools/yast-jenkins-create.rb
@@ -10,7 +10,8 @@ require "yaml"
 conf = YAML.load(File.read("jenkins.yml"))
 USER = conf["username"]
 PWD  = conf["password"]
-URL_BASE = "https://#{USER}:#{PWD}@ci.opensuse.org".freeze
+URL_BASE = "https://#{USER}:#{PWD}@ci.suse.de".freeze
+puts URL_BASE.inspect
 # URL_BASE = "http://river.suse.de"
 
 # %s is replaced by arguments passed to program
@@ -21,13 +22,15 @@ ARGV.each do |mod|
   response_code = `curl -sL -w "%{http_code}" #{URL_BASE}/job/#{JOB_NAME_PATTERN % mod}/ -o /dev/null`
   next if response_code == "200"
 
+  puts "Creating #{mod} ..."
   FileUtils.rm_f "config.xml.tmp"
   # now modify config.xml to fit given module
   `sed 's/yast-.*\.git/yast-#{mod}.git/' config.xml > config.xml.tmp`
 
   # adress found from https://ci.opensuse.org/api
-  res = `curl -X POST #{URL_BASE}/createItem?name=#{JOB_NAME_PATTERN % mod} " \
-    "--header "Content-Type:application/xml" -d @config.xml.tmp`
-  puts "ERROR: #{res}" if $CHILD_STATUS.exitstatus != 0
+  # link how to generate crumb header is at https://wiki.jenkins.io/display/JENKINS/Remote+access+API#RemoteaccessAPI-CSRFProtection
+  res = `curl --http1.0 -X POST #{URL_BASE}/createItem?name=#{JOB_NAME_PATTERN % mod} \
+    --header "Content-Type:application/xml" --header "Jenkins-Crumb:45b1e2bd56c66ef7a03393c3911eb423" -d @config.xml.tmp`
+  puts "ERROR: #{res}" if $?.exitstatus != 0
   puts "ERROR: Wrong Credentials. \n #{res}" if res =~ /Authentication required/
 end
